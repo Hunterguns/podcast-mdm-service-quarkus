64926badfeb3fd72bfe7086e0ea2a2bca03f0c07
Sandeep Nair <82336209+Hunterguns@users.noreply.github.com>
2024-08-03 02:49:39 +0530

    initial commit

---
+++ src/test/java/org/sandeep/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.sandeep;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
